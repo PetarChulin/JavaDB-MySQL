8e4fb11e77c0b058e7a6c7e0447feec303c38e09
PeterC <petercbg@abv.bg>
2022-09-16 10:35:47 +0300

    Update 01. DataDefinitionAndDataTypes_Exercises.sql

--- 01. DataDefinitionAndDataTypes_Exercises.sql
+++ 01. DataDefinitionAndDataTypes_Exercises.sql
@@ -297,6 +297,7 @@ CREATE TABLE `employees` (
     FOREIGN KEY `employees`(`address_id`)
     REFERENCES `addresses`(`id`)
 );
+# Do not submit creation of database only the insert statements.
 
 INSERT INTO `towns` (`name`)
 VALUES
@@ -315,7 +316,7 @@ VALUES
 
 INSERT INTO `employees` (`first_name`, `middle_name`, `last_name`, `job_title`, `department_id`, `hire_date`,`salary`)
 VALUES
-('Ivan', 'Ivanov', 'Ivanov', '.Net Developer', 4, '2013-02-01', 3500.00),
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00),
 ('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00),
 ('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25),
 ('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00),
