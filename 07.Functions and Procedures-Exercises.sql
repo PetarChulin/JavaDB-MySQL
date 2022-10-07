95b958e70917c3e68ef15114e8d617221bb90d66
PeterC <petercbg@abv.bg>
2022-10-07 14:03:17 +0300

    Update 07.Functions and Procedures-Exercises.sql

--- 07.Functions and Procedures-Exercises.sql
+++ 07.Functions and Procedures-Exercises.sql
@@ -112,16 +112,14 @@ RETURN (SELECT (`sum` *(POWER((1 + yearly_interest_rate), years))));
 END %%
 SELECT ufn_calculate_future_value(1000, 0.5, 5) AS output;
 
-#11.Calculating Interest ?? 50/100
+#11.Calculating Interest
 DELIMITER %%
 CREATE PROCEDURE usp_calculate_future_value_for_account(id INT, interest_rate DECIMAL(19,4))
 BEGIN 
-SELECT ah.id AS id, ah.first_name, ah.last_name, a.balance, 
-CAST(ROUND(a.balance * (POWER((1 + interest_rate),5)), 4) AS CHAR) AS balance_in_5_years FROM account_holders ah
-JOIN accounts a ON a.account_holder_id = ah.id
-WHERE ah.id = id
-LIMIT 1;
-
+SELECT a.id AS id, ah.first_name, ah.last_name, a.balance, 
+ufn_calculate_future_value(a.balance, interest_rate, 5) AS balance_in_5_years FROM accounts a
+JOIN account_holders ah ON a.account_holder_id = ah.id
+WHERE a.id = id;
 END %%
 CALL usp_calculate_future_value_for_account(1, 0.1);
 
